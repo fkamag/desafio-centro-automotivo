32fa26c343f1d8879676cf9c3b097bcbb00509ce
fkamag <fabiokamagian@gmail.com>
2023-09-02 22:52:30 -0300

    criado tabelas

--- README.md
+++ README.md
@@ -1,3 +1,162 @@
 # desafio-centro-automotivo
 
-Iniciando projeto
+Desafio do curso de Ciência de Dados - Criando um Banco de Dados para uma Oficina - conteúdo da DIO
+
+## 1 - Criar o modelo relacional para ser utilizado em um centro automotivo.
+
+Utilizado as seguintes entidades:
+-Cliente
+-Carro
+-Peças
+-Serviços
+-Funcionário
+-Ordem de Serviço
+-Pagamento
+
+## 2 - Criar o script SQL para criação do esquema de banco de dados
+
+## Resolução do desafio proposto
+
+Criado tabela cliente com os seguintes campos:
+- id_customer - id do cliente
+- type_person - tipo do cliente (pode ser PJ ou PF)
+
+Criado tabela de endereços, como cada endereço tem cidade e estado que se repetem, foi criado também uma tabela para cada uma destas entidades, linkando através do id
+- Tabela Estado
+  - id_state: id do estado
+  - state_uf: sigla do estado da federação
+- Tabela Cidade
+  - id_city: id da cidade
+  - city_name: nome da cidade
+  - id_state: id do estado para linkar com a tabela de estados
+- Tabela de endereços
+  - id_address: id do endereço
+  - address: logradouro
+  - number: número da rua
+  - district: bairro
+  - zip: CEP
+  - id_city: id da cidade para linkar com a tabela de cidades
+  - id_customer: id do cliente para linkar com a tabela de clientes
+  - alias_address: apelido do endereço Ex. casa, trabalho
+
+  Criado tabela pessoa física (natural_person):
+- id_natural_person: id da pessoa física
+- id_customer: id do cliente linkar com a tabela cliente
+- alias_name: apelido do cliente
+- full_name: nome completo do cliente
+- cpf: único para cada cliente cadastrado
+- birth_date: data de nascimento
+- phone: número de telefone
+
+Criado tabela pessoa jurídica (legal_person):
+- id_legal_person: id da pessoa jurídica
+- id_customer: id do cliente linkar com a tabela cliente
+- trade: nome fantasia
+- company: razão social
+- cnpj: único para cada fornecedor cadastrado
+- phone: número de telefone
+
+Criado tabela de fornecedores, e utilizando as tabelas de cidade e estado já criada na etapa anterior
+- id_supplier: id do fornecedor
+- trade: nome fantasia
+- company: razão social
+- cnpj: único para cada fornecedor cadastrado
+- phone: número de telefone
+- address: logradouro
+- number: número da rua
+- district: bairro
+- zip: CEP
+- id_city: id da cidade para linkar com a tabela de cidades
+
+Criado tabela de categoria de produtos
+- id_category: id da categoria de produtos
+- name_category: nome da categoria
+
+Criado tabela de produtos
+- id_product: id do produto
+- name_product: nome do produto
+- unit_value: valor unitário do produto
+- id_category: id da categoria para linkar com a tabela de categorias de produtos
+- id_supplier: id do fornecedor para linkar com a tabela de fornecedores
+
+Criado tabela de pagamentos
+- id_payment - id da forma de pagamento
+- name_payment - tipo da forma de pagamento
+
+Criado tabela de funcionários
+- id_employee: id do funcionário
+- alias_name: apelido
+- full_name: nome completo
+- cpf: único para cada funcionário
+- birth_date: data de nascimento
+- phone: telefone
+
+Criado tabela de serviços
+- id_service: id do serviço
+- name_service: nome do serviço
+- value_service: valor do serviço
+
+Criado tabela de status da ordem de Serviço
+- id_status_order: id do status da OS
+- name_status_order: nome do status da OS
+
+Criado tabela de veículos
+- id_vehicle: id do veículo
+- id_customer: id do cliente
+- car_brand: marca do veículo
+- car_model: modelo do veículo
+- license_plate: placa do veículo
+
+Criado tabela de ordem de serviço
+- id_order: id da OS
+- id_vehicle: id do veículo linkar com tabela de veículos
+- id_status_order: id do status da OS linkar com tabela de status da OS
+- value_order: valor total da OS
+- date_order: data do pedido
+
+Criado tabela OS x forma de pagamento:
+id_order: id da OS
+id_payment: id da forma de pagamento
+value_payment: valor do pagamento
+
+Criado tabela OS x produto
+para que cada OS possa ter mais de um produto
+- id_order: id da OS linkar com tabela de OS
+- id_product: id do produto linkar com tabela de produtos
+- quantity: quantidade de produto
+- unit_value: valor unitário do produto
+
+Criado tabela OS x serviço
+- id_order: id da OS linkar com tabela de OS
+- id_service: id do serviço linkar com tabela de serviços
+- id_employee: id do funcionário responsável pelo serviço
+- quantity: quantidade de serviço
+- unit_value: valor unitário do serviço
+
+Criado tabela estoque
+- id_stock: id do estoque
+- locality: local do estoque
+
+Criado tabela estoque x produto
+- id_stock: id do estoque linkar com a tabela estoque
+- id_product: id do produto linkar com a tabela produto
+- quantity: quantidade de produto em estoque
+
+<img src="EER-autocenter.png">
+
+
+## 3 - Persistência de dados para testes
+
+Utilizado arquivo inserts.sql para popular o banco de dados.
+
+## 4 - Recuperação de informações com queries SQL
+
+- relação de fornecedores por ordem alfabética com máscara no CNPJ e Telefone
+- relatório de produtos por ordem de fornecedor e quantidade em estoque
+- Resumo das vendas de um determinado dia com quantidade de pedidos, valor total dos pedidos, ticket médio, menor valor de pedido e maior valor de pedido
+- Quantidade de pedidos por consumidor
+- Pedidos com mais de uma forma de pagamento
+
+
+## Considerações Finais
+Foi utilizado o MYSQL Workbench com uma imagem docker mysql para realizar os testes.
---
+++ create.sql
@@ -0,0 +1,163 @@
+DROP DATABASE IF EXISTS autocenter;
+CREATE DATABASE autocenter;
+USE autocenter;
+CREATE TABLE customer (
+	id_customer INT AUTO_INCREMENT PRIMARY KEY,
+    type_person ENUM('NP', 'LP')
+);
+CREATE TABLE state (
+	id_state INT AUTO_INCREMENT PRIMARY KEY,
+    state_uf CHAR(2) NOT NULL
+);
+CREATE TABLE city (
+	id_city INT AUTO_INCREMENT PRIMARY KEY,
+    city_name VARCHAR(50) NOT NULL,
+    id_state INT,
+    CONSTRAINT fk_state FOREIGN KEY (id_state) REFERENCES state (id_state)
+);
+CREATE TABLE natural_person (
+	id_natural_person INT AUTO_INCREMENT PRIMARY KEY,
+    id_customer INT,
+    alias_name VARCHAR(20)  NOT NULL,
+    full_name VARCHAR(50) NOT NULL,
+    cpf CHAR(11) NOT NULL,
+    birth_date DATE NOT NULL,
+    phone VARCHAR(11),
+    CONSTRAINT unique_cpf_costumer UNIQUE(cpf),
+    CONSTRAINT fk_natural_person FOREIGN KEY (id_customer) REFERENCES customer (id_customer)
+);
+CREATE TABLE legal_person (
+	id_legal_person INT AUTO_INCREMENT PRIMARY KEY,
+    id_customer INT,
+    trade_supplier VARCHAR(30)  NOT NULL,
+    company_supplier VARCHAR(50) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
+    phone VARCHAR(11),
+    CONSTRAINT unique_cnpj_consumer UNIQUE(cnpj),
+    CONSTRAINT fk_legal_person FOREIGN KEY (id_customer) REFERENCES customer (id_customer)
+);
+CREATE TABLE address_customer (
+	id_address INT AUTO_INCREMENT PRIMARY KEY,
+    address_name VARCHAR(80) NOT NULL,
+    address_number INT,
+    district VARCHAR(50),
+    zip CHAR(8),
+    id_city INT,
+    id_customer INT,
+    alias_address VARCHAR(20),
+    CONSTRAINT fk_city FOREIGN KEY (id_city) REFERENCES city (id_city),
+    CONSTRAINT fk_customer FOREIGN KEY (id_customer) REFERENCES customer (id_customer)
+);
+CREATE TABLE supplier (
+	id_supplier INT AUTO_INCREMENT PRIMARY KEY,
+    trade_supplier VARCHAR(30)  NOT NULL,
+    company_supplier VARCHAR(50) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
+    address_name VARCHAR(80) NOT NULL,
+    address_number INT,
+    district VARCHAR(50),
+    zip CHAR(8),
+    id_city INT,
+    phone VARCHAR(11),
+    CONSTRAINT unique_cnpj_suplier UNIQUE(cnpj),
+    CONSTRAINT fk_city_supplier FOREIGN KEY (id_city) REFERENCES city (id_city)
+);
+CREATE TABLE category_product (
+	id_category INT AUTO_INCREMENT PRIMARY KEY,
+    name_category VARCHAR(30) NOT NULL
+);
+CREATE TABLE product (
+	id_product INT AUTO_INCREMENT PRIMARY KEY,
+    name_product VARCHAR(30) NOT NULL,
+    unit_value DECIMAL(10,2),
+    id_category INT,
+    id_supplier INT,
+    CONSTRAINT fk_category FOREIGN KEY (id_category) REFERENCES category_product (id_category),
+    CONSTRAINT fk_supplier FOREIGN KEY (id_supplier) REFERENCES supplier (id_supplier)
+);
+CREATE TABLE payment (
+	id_payment INT AUTO_INCREMENT PRIMARY KEY,
+    name_payment VARCHAR(20) NOT NULL
+);
+CREATE TABLE employee (
+	id_employee INT AUTO_INCREMENT PRIMARY KEY,
+    alias_name VARCHAR(20),
+    full_name VARCHAR(50) NOT NULL,
+    cpf CHAR(11) NOT NULL,
+    birth_date DATE NOT NULL,
+    phone VARCHAR(11),
+    CONSTRAINT unique_cpf_employee UNIQUE(cpf)
+);
+CREATE TABLE status_service (
+	id_status_service INT AUTO_INCREMENT PRIMARY KEY,
+    name_status_service VARCHAR(20) NOT NULL
+);
+CREATE TABLE service (
+	id_service INT AUTO_INCREMENT PRIMARY KEY,
+    name_service VARCHAR(50),
+    value_service DECIMAL(10,2),
+    id_status_service INT,
+    CONSTRAINT fk_service_status FOREIGN KEY (id_status_service) REFERENCES status_service (id_status_service)
+);
+CREATE TABLE status_order (
+	id_status_order INT AUTO_INCREMENT PRIMARY KEY,
+    name_status_order VARCHAR(20) NOT NULL
+);
+CREATE TABLE vehicle (
+	id_vehicle INT AUTO_INCREMENT PRIMARY KEY,
+    id_customer INT,
+    car_brand VARCHAR(30),
+    car_model VARCHAR(30),
+    license_plate VARCHAR(7),
+    CONSTRAINT fk_vehicle_customer FOREIGN KEY (id_customer) REFERENCES customer (id_customer),
+    CONSTRAINT unique_vehicle UNIQUE(license_plate)
+);
+CREATE TABLE orders (
+	id_order INT AUTO_INCREMENT PRIMARY KEY,
+    id_vehicle INT,
+    id_status_order INT,
+    value_order DECIMAL(10,2),
+    data_order DATETIME,
+    CONSTRAINT fk_order_vehicle FOREIGN KEY (id_vehicle) REFERENCES vehicle (id_vehicle),
+    CONSTRAINT fk_order_status FOREIGN KEY (id_status_order) REFERENCES status_order (id_status_order)
+);
+CREATE TABLE order_payment (
+	id_order INT,
+    id_payment INT,
+    value_payment DECIMAL(10,2),
+    CONSTRAINT fk_payment_order FOREIGN KEY (id_order) REFERENCES orders (id_order),
+    CONSTRAINT fk_order_payment FOREIGN KEY (id_payment) REFERENCES payment (id_payment),
+    PRIMARY KEY (id_order, id_payment)
+);
+CREATE TABLE order_product (
+	id_order INT,
+    id_product INT,
+    quantity INT,
+    unit_value DECIMAL(5,2),
+    CONSTRAINT fk_order_product FOREIGN KEY (id_order) REFERENCES orders (id_order),
+    CONSTRAINT fk_product_order FOREIGN KEY (id_product) REFERENCES product (id_product),
+    PRIMARY KEY (id_order, id_product)
+);
+CREATE TABLE order_service (
+	id_order INT,
+    id_service INT,
+    id_employee INT,
+    quantity INT,
+    unit_value DECIMAL(5,2),
+    CONSTRAINT fk_order_service FOREIGN KEY (id_order) REFERENCES orders (id_order),
+    CONSTRAINT fk_service_order FOREIGN KEY (id_service) REFERENCES service (id_service),
+    CONSTRAINT fk_service_employee FOREIGN KEY (id_employee) REFERENCES employee (id_employee),
+    PRIMARY KEY (id_order, id_service)
+);
+CREATE TABLE stock (
+	id_stock INT AUTO_INCREMENT PRIMARY KEY,
+    locality VARCHAR(30)
+);
+CREATE TABLE product_stock (
+	id_product INT,
+    id_stock INT,
+    quantity DECIMAL(5,2),
+    CONSTRAINT fk_product_stock FOREIGN KEY (id_product) REFERENCES product (id_product),
+    CONSTRAINT fk_stock_product FOREIGN KEY (id_stock) REFERENCES stock (id_stock),
+    PRIMARY KEY (id_product, id_stock)
+);
